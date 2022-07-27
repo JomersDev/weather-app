6054d5e93a2e61081bdb54d313f29167aeba2e69
JomersDev <james.mcgrory135@gmail.com>
2022-07-27 16:35:08 +1000

    Current weather of any city displaying to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log("test link");
+const searchBar = document.getElementById('search');
+const submitButton = document.getElementById('submitSearch');
+const API_KEY = 'd5593e4a1e2f16160e3c632f71177701';
+
+submitButton.addEventListener('click', getWeatherData);
+
+
+
+async function weatherForecast() {
+    try {
+        const city = searchBar.value;
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric` , {mode: 'cors'});
+        const weatherData = await response.json();
+        const forecast = weatherData.list;
+        console.log(forecast);
+    } catch (err) {
+        console.log('error in fetching weather data' , err);
+    }
+}
+
+async function currentWeather() {
+    try {
+        const city = searchBar.value;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric` , {mode: 'cors'});
+        const currentWeatherData = await response.json();
+        const currentWeather = currentWeatherData.main;
+        console.log(currentWeather);
+        console.log(`${city} is currently: `, currentWeather.temp)
+    } catch (err) {
+        console.log('error in fetching weather data' , err);
+    }
+}
+
+function getWeatherData() {
+    weatherForecast();
+    currentWeather();
+}
+
+
+
+
